b51b40da2b31a3d9eed5c35f2044d48492d6db18
MrSurvival <74323031+MrSurvival@users.noreply.github.com>
2021-02-23 09:19:25 +0100

    add

---
+++ logischeoperatoren.php
@@ -0,0 +1,41 @@
+<?php
+$var1= 5;
+$var2 = 10;
+
+// Aufgabe 2.1
+if($var1==$var2) {  //wenn wahr (true/1)// tue etwas} else  {  // wenn falsch (false/0)// tue etwas anderes}
+    echo "Ist Wahr!";
+}
+else{
+    echo "Ist Falsch!"."<br>";
+}
+
+// Aufgabe 2.2
+if ($var1==$var2) {
+    echo " Var = Var2"."<br>";
+}
+elseif ($var1!=$var2){
+    echo " Var != Var2"."<br>";
+}
+elseif ($var1>$var2){
+    echo " Var > Var2"."<br>";
+}
+elseif ($var1<$var2){
+    echo " Var < Var2"."<br>";
+}
+
+//Aufgabe 2.3
+if ($var1==$var2) && ($var1<$var2){
+    echo "und ist Wahr!"."<br>";
+}
+else{
+    echo "und ist Falsch!"."<br>";
+}
+
+if ($var1==$var2) || ($var1<$var2){
+    echo "und ist Wahr!"."<br>";
+}
+else{
+    echo "und ist Falsch!"."<br>";
+}
+?>
