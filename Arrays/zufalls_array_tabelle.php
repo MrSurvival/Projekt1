34423cc83c775b6c686c0860b5abcffc592409d5
MrSurvival <74323031+MrSurvival@users.noreply.github.com>
2021-06-08 09:49:30 +0200

    add

--- Arrays/zufalls_array_tabelle.php
+++ Arrays/zufalls_array_tabelle.php
@@ -5,5 +5,7 @@
         $zufall = rand(0,100); // eine Zufallszahl zwischen 1 und 100
         $feld[$i] = $zufall; // Schreibe die Zahl in das i-te Feld
         echo "Index:[".$i."] = ".$zufall."<br>";
+
+        //http://localhost/Schule_11/Projekt1/Arrays/zufalls_array_tabelle.php
     }
 ?>
